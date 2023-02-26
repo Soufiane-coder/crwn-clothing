318f351448df2e54cd8cccf2752ed97e9b834501
Soufiane-coder <84711969+Soufiane-coder@users.noreply.github.com>
2023-02-26 15:26:10 +0100

    implemented firevase utils, included ability to store authenticated user

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 import './sign-in-and-sign-up.scss'
 
 const SignInAndSignUpPage = () => (
-    <div className="sing-in-and-sign-up">
-        <SignIn/>
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+        <SignUp />
     </div>
 )
 export default SignInAndSignUpPage;
